21080661bbb2f1bf5b09a53fca6b21e36d90cea8
Sahabe Alam <sahabeip@gmail.com>
2021-09-11 17:29:59 +0530

    first commit

---
+++ ComposeSample/ComposeSample/ComposeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposeSampleApp.swift
+//  ComposeSample
+//
+//  Created by Sahabe.Alam on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ComposeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposeSample/ComposeSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComposeSample
+//
+//  Created by Sahabe.Alam on 11/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
